24aec17b9dead13ec4468eb6e71438293bea6bb9
Luceva2002 <luceva2002@gmail.com>
2025-03-07 15:10:31 +0100

    get_next_line_bonus

---
+++ get_next_line_bonus.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: luevange <luevange@student.42roma.it>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/07 15:08:23 by luevange          #+#    #+#             */
+/*   Updated: 2025/03/07 15:08:27 by luevange         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE 250
+#endif
+
+char *ft_strlcpy(char *s1, char *s2, size_t len);
+char *get_next_line(int fd);
+char *ft_strjoin(char *s1, char *s2);
+int ft_strchar(const char *s, int c);
+size_t ft_strlen(const char *s);
+
+#endif
